43bfbe8676e29c46f19571f325d5bf9ef39759aa
Verline <verlineguo34@gmail.com>
2025-05-15 22:20:46 +0700

    error

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -101,10 +101,8 @@
 	<!-- Bootstrap JS -->
 	<script src="{{ asset("backend") }}/assets/js/bootstrap.bundle.min.js"></script>
 	<!--plugins-->
-	<script src="{{ asset("backend") }}/assets/js/jquery.min.js"></script>
 	<script src="{{ asset("backend") }}/assets/plugins/simplebar/js/simplebar.min.js"></script>
 	<script src="{{ asset("backend") }}/assets/plugins/metismenu/js/metisMenu.min.js"></script>
-	<script src="{{ asset("backend") }}/assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
 	<!--Password show & hide js -->
 	<script>
 		$(document).ready(function () {
